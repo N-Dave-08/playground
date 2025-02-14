ab2aa0b15124e0720f361f33c417dd87885ccf6e
N-Dave-08 <cdave.dev@gmail.com>
2025-02-15 03:38:57 +0800

    updated the color, and navigate to login after register

--- crud/src/app/login/page.tsx
+++ crud/src/app/login/page.tsx
@@ -17,7 +17,7 @@ export default function LoginPage() {
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+      <div className="max-w-md w-full p-6 bg-neutral text-neutral-content rounded-lg shadow-lg">
         <h1 className="text-2xl font-bold text-center mb-6">Login</h1>
         <LoginForm />
       </div>
--- crud/src/app/register/page.tsx
+++ crud/src/app/register/page.tsx
@@ -11,13 +11,13 @@ export default function RegisterPage() {
 
   useEffect(() => {
     if (isAuthenticated) {
-      router.push('/');
+      router.push('/login');
     }
   }, [isAuthenticated, router]);
 
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+      <div className="max-w-md w-full p-6 bg-neutral text-neutral-content rounded-lg shadow-lg">
         <h1 className="text-2xl font-bold text-center mb-6">Register</h1>
         <RegisterForm />
       </div>
